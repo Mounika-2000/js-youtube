c8650484388c889c0add9f5784d3e27ef0bd77c7
Mounika-2000 <116418385+Mounika-2000@users.noreply.github.com>
2025-03-31 14:23:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score=33
+let num= Number(score)
+// console.log(typeof(num));
+// console.log(typeof num);
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let IsLoggedIn = 1
+let booleanIsLoggedIn= Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => fasle
+// "Mounika" => true
+
+let someNum = 33
+let stringSomeNum = String(someNum)
+console.log(stringSomeNum);
+console.log(typeof stringSomeNum);
