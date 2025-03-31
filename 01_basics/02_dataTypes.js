32c629df6abd2e2c00387793fb12415d506577c1
Mounika-2000 <116418385+Mounika-2000@users.noreply.github.com>
2025-03-31 12:03:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)        //has a different syntax since it is nodejs and not browser
+console.log(3
+    +3) // code readability should be high
+console.log("Mounika")
+
+let name="Mounika" //string
+let age=20 // number
+let loggedIn=true //boolean
+let state;
+
+// primitive data types
+// number => 2^53
+// bigint > 2^53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned yet
+// symbol => unique
+
+//object
+console.log(typeof "Mounika");
+console.log(typeof age);
+console.log(typeof null);          // object
+console.log(typeof undefined);     // undefined
