1dff4b91c7bb644adfde8028f5cd58ae12abf674
Mounika-2000 <116418385+Mounika-2000@users.noreply.github.com>
2025-04-01 10:45:24 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,5 +18,54 @@ let booleanIsLoggedIn= Boolean(IsLoggedIn)
 
 let someNum = 33
 let stringSomeNum = String(someNum)
-console.log(stringSomeNum);
-console.log(typeof stringSomeNum);
+// console.log(stringSomeNum);
+// console.log(typeof stringSomeNum);
+
+// ********************** Operations *************************
+
+let value=3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1 = "hello"
+let str2 = " mounika"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2");  //12
+// console.log("1" + 1 + 2); //112 decides which type based on 1st occuring type
+// console.log(1 + 1 + "2"); //32
+
+// console.log(1+2*7/2%3);  // dont use such statements without brackets
+
+// console.log(+true); //1
+// console.log(+false); //0
+// console.log(+""); //0
+
+let num1,num2,num3
+num1,num2,num3=2+2
+
+let gameCounter=100
+gameCounter++
+// console.log(gameCounter); //101
+
+++gameCounter
+// console.log(gameCounter); //102
+
+let x=3
+const y=x++
+console.log(`x:${x}, y:${y}`); // single quotes under esc
+
+let a=3
+const b=++a
+console.log(`a:${a}, b:${b}`);
+
+
