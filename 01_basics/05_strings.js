e520736cfe3842d21b27f6cb6b22f26fc642ffa1
Mounika-2000 <116418385+Mounika-2000@users.noreply.github.com>
2025-04-04 06:32:12 +0000

    string operations

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,7 +1,7 @@
 const name="mounika"
 const repoCount=50
 // console.log(name+repoCount+" value");
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repository count is ${repoCount}`);
 
 const gameName=new String('mounika-ds-com')
 // console.log(gameName[0]);
