ea27dd65be4bbc6c7f1d69f7e880de96b774dc01
Mounika-2000 <116418385+Mounika-2000@users.noreply.github.com>
2025-04-11 16:51:07 +0000

    control flow(if, if else)

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// conditional flow
+// if
+const isUserLoggedIn=true
+const temp=41
+
+// if(temp<50){           // ==
+//     console.log("temperature is less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+// >, <, <=, >=, !=, ==, ===, !==
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+
+// const balance=1000
+// if(balance>500) console.log("test");        // implicit scope: one line execution
+// if(balance>500) console.log("test"), console.log("test2");   // not a good practice
+
+// if(balance<500){
+//     console.log("less than 500");
+// }else if (balance<750){
+//     console.log("less than 750");
+// }else if (balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const isLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+if(isLoggedIn&&debitCard&&(2==3)){
+    console.log("allow to buy course");  
+}
+if(loggedInFromGoogle||loggedInFromEmail){
+    console.log("user logged in");
+}
