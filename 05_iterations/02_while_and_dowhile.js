25a249fd4bd06241082b8a7491c04f0b66297055
Mounika-2000 <116418385+Mounika-2000@users.noreply.github.com>
2025-04-14 10:16:02 +0000

    while and do while

---
+++ 05_iterations/02_while_and_dowhile.js
@@ -0,0 +1,18 @@
+let index=0
+while (index<=10) {
+    // console.log(`value is: ${index}`);
+    index=index+2
+}
+
+let myArr=['flash','batman','superman']
+let i=0
+while (i<myArr.length) {
+    // console.log(`value is: ${myArr[i]}`);
+    i=i+1
+}
+
+let score=11
+do {
+    console.log(`score is: ${score}`);
+    
+} while (score<=10);
